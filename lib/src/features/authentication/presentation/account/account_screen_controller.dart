1b22eb08e5f2b3c52617fed2766f9053c9b9a326
Alex Martynov <martynov.alex@gmail.com>
2024-07-26 16:46:10 +0600

    Create AccountScreenController

---
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nice_and_healthy/src/features/authentication/data/fake_auth_repository.dart';
+
+class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+  AccountScreenController({required this.authRepository})
+      : super(const AsyncValue.data(null));
+
+  final FakeAuthRepository authRepository;
+
+  Future<void> signOut() async {
+    // set state to loading
+    // sign out (using auth repo)
+    // if success, set state to data(null)
+    // if error, set state to error
+    try {
+      state = const AsyncValue<void>.loading();
+      await authRepository.signOut();
+      state = const AsyncValue<void>.data(null);
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+    }
+  }
+}
+
+final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
+    AccountScreenController, AsyncValue<void>>((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AccountScreenController(authRepository: authRepository);
+});
