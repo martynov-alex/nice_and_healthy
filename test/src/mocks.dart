41dbacab8cc7b4e6361d254168f97dd2858d5bbc
Alex Martynov <martynov.alex@gmail.com>
2024-08-14 17:03:12 +0600

    Add AddToCartController tests

--- test/src/mocks.dart
+++ test/src/mocks.dart
@@ -1,6 +1,7 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:nice_and_healthy/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:nice_and_healthy/src/features/authentication/domain/app_user.dart';
+import 'package:nice_and_healthy/src/features/cart/application/cart_service.dart';
 import 'package:nice_and_healthy/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:nice_and_healthy/src/features/cart/data/remote/remote_cart_repository.dart';
 
@@ -16,3 +17,5 @@ class MockAuthRepository extends Mock implements FakeAuthRepository {}
 class MockRemoteCartRepository extends Mock implements RemoteCartRepository {}
 
 class MockLocalCartRepository extends Mock implements LocalCartRepository {}
+
+class MockCartService extends Mock implements CartService {}
