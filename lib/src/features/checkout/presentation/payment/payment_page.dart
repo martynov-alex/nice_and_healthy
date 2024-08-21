9f664269e2e956b5b1b36184ce12c1f3504b953c
Alex Martynov <martynov.alex@gmail.com>
2024-08-21 16:45:04 +0600

    Updated the PaymentPage

--- lib/src/features/checkout/presentation/payment/payment_page.dart
+++ lib/src/features/checkout/presentation/payment/payment_page.dart
@@ -1,41 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:nice_and_healthy/src/features/checkout/presentation/payment/payment_button.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nice_and_healthy/src/common_widgets/async_value_widget.dart';
+import 'package:nice_and_healthy/src/features/cart/application/cart_service.dart';
+import 'package:nice_and_healthy/src/features/cart/domain/cart.dart';
 import 'package:nice_and_healthy/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart';
 import 'package:nice_and_healthy/src/features/cart/presentation/shopping_cart/shopping_cart_items_builder.dart';
-import 'package:nice_and_healthy/src/features/cart/domain/item.dart';
+import 'package:nice_and_healthy/src/features/checkout/presentation/payment/payment_button.dart';
 
 /// Payment screen showing the items in the cart (with read-only quantities) and
 /// a button to checkout.
-class PaymentPage extends StatelessWidget {
+class PaymentPage extends ConsumerWidget {
   const PaymentPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: Listen to cart changes on checkout and update the UI.
-    // TODO: Read from data source
-    const cartItemsList = [
-      Item(
-        productId: '1',
-        quantity: 1,
-      ),
-      Item(
-        productId: '2',
-        quantity: 2,
-      ),
-      Item(
-        productId: '3',
-        quantity: 3,
-      ),
-    ];
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cartValue = ref.watch(cartProvider);
 
-    return ShoppingCartItemsBuilder(
-      items: cartItemsList,
-      itemBuilder: (_, item, index) => ShoppingCartItem(
-        item: item,
-        itemIndex: index,
-        isEditable: false,
-      ),
-      ctaBuilder: (_) => const PaymentButton(),
+    return AsyncValueWidget<Cart>(
+      value: cartValue,
+      data: (cart) {
+        return ShoppingCartItemsBuilder(
+          items: cart.toItemsList(),
+          itemBuilder: (_, item, index) => ShoppingCartItem(
+            item: item,
+            itemIndex: index,
+            isEditable: false,
+          ),
+          ctaBuilder: (_) => const PaymentButton(),
+        );
+      },
     );
   }
 }
