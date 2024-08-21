f07a858fb0b14854b252886d2ffb00f9a6525f41
Alex Martynov <martynov.alex@gmail.com>
2024-08-21 16:03:29 +0600

    Add some files for Orders feature

--- lib/src/features/checkout/presentation/checkout_screen/checkout_screen.dart
+++ lib/src/features/checkout/presentation/checkout_screen/checkout_screen.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:nice_and_healthy/src/features/checkout/presentation/payment/payment_page.dart';
 import 'package:nice_and_healthy/src/features/authentication/presentation/sign_in/email_password_sign_in_screen.dart';
 import 'package:nice_and_healthy/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
+import 'package:nice_and_healthy/src/features/checkout/presentation/payment/payment_page.dart';
 import 'package:nice_and_healthy/src/localization/string_hardcoded.dart';
 
 /// The two sub-routes that are presented as part of the checkout flow.
-/// TODO: add the address page as well (see [AddressScreen]).
 enum CheckoutSubRoute { register, payment }
 
 /// This is the root widget of the checkout flow, which is composed of 2 pages:
 /// 1. Register page
 /// 2. Payment page
-/// The correct page is displayed (and updated) based on whether the user is
-/// signed in.
-/// The logic for the entire flow is implemented in the
-/// [CheckoutScreenController], while UI updates are handled by a
-/// [PageController].
+// TODO: Show the correct page based on whether the user is signed in.
 class CheckoutScreen extends StatefulWidget {
   const CheckoutScreen({super.key});
 
@@ -27,6 +22,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
   final _controller = PageController();
 
   var _subRoute = CheckoutSubRoute.register;
+  // TODO: Load the correct initial page when this screen is presented
 
   @override
   void dispose() {
