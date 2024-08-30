ee11657ac2b597e6e12381f2f80c38b6ed4baad5
Alex Martynov <martynov.alex@gmail.com>
2024-08-30 13:24:27 +0600

    Add updateProductRating and tests

--- lib/src/features/reviews/application/reviews_service.dart
+++ lib/src/features/reviews/application/reviews_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nice_and_healthy/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:nice_and_healthy/src/features/products/data/fake_products_repository.dart';
 import 'package:nice_and_healthy/src/features/products/domain/product.dart';
 import 'package:nice_and_healthy/src/features/reviews/data/fake_reviews_repository.dart';
 import 'package:nice_and_healthy/src/features/reviews/domain/review.dart';
@@ -25,6 +26,37 @@ class ReviewsService {
           uid: user.uid,
           review: review,
         );
+    // * Note: this should be done on the backend
+    // * At this stage the review is already submitted
+    // * and we don't need to await for the product rating to also be updated
+    _updateProductRating(productId);
+  }
+
+  Future<void> _updateProductRating(ProductID productId) async {
+    final reviews =
+        await ref.read(reviewsRepositoryProvider).fetchReviews(productId);
+    final avgRating = _avgReviewScore(reviews);
+    final product = ref.read(productsRepositoryProvider).getProduct(productId);
+    if (product == null) {
+      throw StateError('Product not found with id: $productId.'.hardcoded);
+    }
+    final updated = product.copyWith(
+      avgRating: avgRating,
+      numRatings: reviews.length,
+    );
+    await ref.read(productsRepositoryProvider).setProduct(updated);
+  }
+
+  double _avgReviewScore(List<Review> reviews) {
+    if (reviews.isNotEmpty) {
+      var total = 0.0;
+      for (var review in reviews) {
+        total += review.rating;
+      }
+      return total / reviews.length;
+    } else {
+      return 0.0;
+    }
   }
 }
 
